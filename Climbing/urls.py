4caa7919867403808f9a1460e35a37123ebe10ad
Spencer Cheng <89827794+spencercheng@users.noreply.github.com>
2022-10-31 14:52:02 -0600

    Create more routes

--- Climbing/urls.py
+++ Climbing/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView, aboutPageView, contactPageView
 
 urlpatterns = [
-    path("", indexPageView, name="index")    
+    path("", indexPageView, name="index"), 
+    path("about/", aboutPageView, name="about" ),
+    path("contact/" , contactPageView, name="contact"),
 ]     
--- Climbing/views.py
+++ Climbing/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 def indexPageView(request) :
     return HttpResponse('Hello Universe!')  
 
+def aboutPageView(request):
+    return HttpResponse("About page")
+
+def contactPageView(request):
+    return HttpResponse("Contact Page")
