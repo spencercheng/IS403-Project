173d95d0ae7f080983e52d5cac2d72c56759cf32
ikemelanson <ikemelanson@gmail.com>
2022-12-12 14:50:29 -0700

    edit and del forms

--- Climbing/urls.py
+++ Climbing/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import indexPageView, routesPageView, learnMorePageView, aboutPageView, editPageView, addPageView
+from .views import *
 
 urlpatterns = [
     path("", indexPageView, name="index"), 
@@ -8,4 +8,13 @@ urlpatterns = [
     path("about/", aboutPageView, name="about"),
     path("edit/", editPageView, name="edit"),
     path("add/", addPageView, name="add"),
+    path("viewCrags/", viewCrags, name="viewCrags"),
+    path("viewWalls/", viewWalls, name="viewWalls"),
+    path("viewRoutes/", viewRoutes, name="viewRoutes"),
+    path("editCrag/", editCrag, name="editCrag"),
+    path("editWall/", editWall, name="editWall"),
+    path("editRoute/", editRoute, name="editRoute"),
+    path("delCrag/", delCrag, name="delCrag"),
+    path("delWall/", delWall, name="delWall"),
+    path("delRoute/", delRoute, name="delRoute"),
 ]     
--- Climbing/views.py
+++ Climbing/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 def indexPageView(request) :
     return render(request, 'Climbing/index.html')
@@ -18,3 +19,83 @@ def editPageView(request):
      
 def addPageView(request):
     return render(request, 'Climbing/add.html')
+
+def viewCrags(request):
+    
+    cragData = Crag.objects.all()
+
+    context = {
+        "cragData" : cragData
+    }
+
+    return render(request, "Climbing/ViewCrags.html", context)
+
+def viewWalls(request):
+    
+    wallData = Wall.objects.all()
+
+    context = {
+        "wallData" : wallData
+    }
+
+    return render(request, "Climbing/viewWalls.html", context)
+
+def viewRoutes(request):
+    
+    routeData = Route.objects.all()
+
+    context = {
+        "routeData" : routeData
+    }
+
+    return render(request, "Climbing/viewRoutes.html", context)
+
+def editCrag(request):
+    crag = Crag.objects.get(id = request.POST['cragID'])
+
+    context = {
+        'crag' : crag
+    }
+
+    return render(request, "Climbing/editCrag.html", context)
+
+def editWall(request):
+    wall = Wall.objects.get(id = request.POST['wallID'])
+
+    context = {
+        'wall' : wall
+    }
+
+    return render(request, "Climbing/editWall.html", context)
+
+def editRoute(request):
+    route = Route.objects.get(id = request.POST['routeID'])
+
+    context = {
+        'route' : route
+    }
+
+    return render(request, "Climbing/editRoute.html", context)
+
+
+def delCrag(request):
+
+    crag = Crag.objects.get(id = request.POST['cragID'])
+    crag.delete()
+
+    return redirect("viewCrags")
+
+
+def delWall(request):
+
+    wall = Wall.objects.get(id = request.POST['wallID'])
+    wall.delete()
+
+    return redirect("viewWalls")
+
+def delRoute(request):
+
+    route = Route.objects.get(id = request.POST['routeID'])
+    route.delete()
+
+    return redirect("viewRoutes")
